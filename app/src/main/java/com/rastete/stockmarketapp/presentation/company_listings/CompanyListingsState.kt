f4dab0463e9c14772c52ab6cdd35d32f8960195b
Raul Emilio Astete Acuña <rastete.dev@gmail.com>
2022-05-24 08:48:21 -0500

    CompanyListingsViewModel

---
+++ app/src/main/java/com/rastete/stockmarketapp/presentation/company_listings/CompanyListingsState.kt
@@ -0,0 +1,10 @@
+package com.rastete.stockmarketapp.presentation.company_listings
+
+import com.rastete.stockmarketapp.domain.model.CompanyListing
+
+data class CompanyListingsState(
+    val companies: List<CompanyListing> = emptyList(),
+    val isLoading: Boolean = false,
+    val isRefreshing: Boolean = false,
+    val searchQuery: String = ""
+)
